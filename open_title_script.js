6764957b37b40ec8fe48bd2278341903da8516e1
jimturnerstudio@gmail.com <jimturnerstudio@gmail.com>
2023-10-16 09:30:49 -0600

    added opening title to initial load of the website

---
+++ open_title_script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        window.location.href = "main_index.html"; // Replace "main.html" with the desired URL
+    }, 2000); // 2000 milliseconds = 2 seconds (duration of the fade-out animation)
+});
+
