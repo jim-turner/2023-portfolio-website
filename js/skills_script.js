a0c6030754afaa4253a82386594b773caa93ea8f
jimturnerstudio@gmail.com <jimturnerstudio@gmail.com>
2023-10-18 13:25:47 -0600

    hamburger menu now functional

--- js/skills_script.js
+++ js/skills_script.js
@@ -1,16 +1,31 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menuCover = document.querySelector('.menu-cover');
-const menuPopup = document.querySelector('.menu-popup');
-const closeButton = document.querySelector('.close-button');
 
-hamburgerMenu.addEventListener('click', () => {
-    menuCover.classList.toggle('active');
-    menuPopup.classList.toggle('active');
-    document.body.classList.toggle('no-scroll');
-});
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-closeButton.addEventListener('click', () => {
-    menuCover.classList.remove('active');
-    menuPopup.classList.remove('active');
-    document.body.classList.remove('no-scroll');
-});
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+
+// const hamburgerMenu = document.querySelector('.hamburger-menu');
+// const menuCover = document.querySelector('.menu-cover');
+// const menuPopup = document.querySelector('.menu-popup');
+
+// hamburgerMenu.addEventListener('click', () => {
+//     menuCover.classList.toggle('active');
+//     menuPopup.classList.toggle('active');
+//     document.body.classList.toggle('no-scroll');
+// });
+
+// // Close the menu when clicking outside the menu popup
+// document.addEventListener('click', (event) => {
+//     if (!menuPopup.contains(event.target) && !hamburgerMenu.contains(event.target)) {
+//         menuCover.classList.remove('active');
+//         menuPopup.classList.remove('active');
+//         document.body.classList.remove('no-scroll');
+//     }
+// });
