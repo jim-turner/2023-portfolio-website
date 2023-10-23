2f65fcdaad835414e458d545d493308bd48eb74d
jimturnerstudio@gmail.com <jimturnerstudio@gmail.com>
2023-10-23 10:27:31 -0600

    skill page changes

--- js/skills_script.js
+++ js/skills_script.js
@@ -1,31 +1,23 @@
-
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-
-
-// const hamburgerMenu = document.querySelector('.hamburger-menu');
-// const menuCover = document.querySelector('.menu-cover');
-// const menuPopup = document.querySelector('.menu-popup');
-
-// hamburgerMenu.addEventListener('click', () => {
-//     menuCover.classList.toggle('active');
-//     menuPopup.classList.toggle('active');
-//     document.body.classList.toggle('no-scroll');
-// });
-
-// // Close the menu when clicking outside the menu popup
-// document.addEventListener('click', (event) => {
-//     if (!menuPopup.contains(event.target) && !hamburgerMenu.contains(event.target)) {
-//         menuCover.classList.remove('active');
-//         menuPopup.classList.remove('active');
-//         document.body.classList.remove('no-scroll');
-//     }
-// });
+const themeMap = {
+    dark: "light",
+    light: "solar",
+    solar: "dark"
+  };
+  
+  const theme = localStorage.getItem('theme')
+    || (tmp = Object.keys(themeMap)[0],
+        localStorage.setItem('theme', tmp),
+        tmp);
+  const bodyClass = document.body.classList;
+  bodyClass.add(theme);
+  
+  function toggleTheme() {
+    const current = localStorage.getItem('theme');
+    const next = themeMap[current];
+  
+    bodyClass.replace(current, next);
+    localStorage.setItem('theme', next);
+  }
+  
+  document.getElementById('themeButton').onclick = toggleTheme;
+  
