1c3ab5ae16837688bc9c5931d5e0fca17d6fb10b
jimturnerstudio@gmail.com <jimturnerstudio@gmail.com>
2023-10-25 09:56:55 -0600

    made changes to skills page

--- js/skills_script.js
+++ js/skills_script.js
@@ -27,6 +27,8 @@ function openModal() {
   document.getElementById("myModal").style.display = "block";
 }
 
+
+
 // Close the Modal
 function closeModal() {
   document.getElementById("myModal").style.display = "none";
@@ -62,3 +64,4 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
